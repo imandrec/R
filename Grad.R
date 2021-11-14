d60dd8312671e16ca59a060c69592fc00647e533
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-11-14 15:45:28 -0600

    Create Grad.R

---
+++ Grad.R
@@ -0,0 +1,29 @@
+#
+#install.packages("ggplot2")
+#install.packages("quantmod")
+#install.packages("magrittr")
+#library(ggplot2)
+#library(quantmod)
+#library(magrittr)
+#getSymbols("MMM", src = "yahoo", from = "2001-11-01", to = "2021-11-01")
+#summary(MMM)
+#candleChart(MMM, up.col = "green", dn.col = "red", theme = "white")
+
+#
+#install.packages("ggplot2")
+#install.packages("quantmod")
+#install.packages("magrittr")
+library(ggplot2)
+library(quantmod)
+library(magrittr)
+#download closing prices 20 stocks
+tickers = c("JNJ", "CBNK", "MMM", "CAT", "AAPL", "NFLX", "AMZN", "GOOG", "PEP", "FB", "NVDA", "BABA", "ACC", "AAL", "ABNB", "ADBE", "ADSK", "APR", "ASTE", "ATHE")
+#get from last 20 years
+getSymbols(tickers, src = "yahoo", from = "2001-11-01", to = "2021-11-01")
+
+summary(MMM)
+#plot
+candleChart(MMM, up.col = "green", dn.col = "red", theme = "white")
+#calculate the 10-,50-,200 moving averages 
+candleChart(MMM, up.col = "green", dn.col = "red", theme = "white")
+addSMA(n = c(20, 50, 200))
