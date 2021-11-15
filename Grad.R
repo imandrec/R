48c7b8f872105c13138487da070e796dc410b27c
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-11-15 07:22:00 -0600

    Update Grad.R

--- Grad.R
+++ Grad.R
@@ -20,10 +20,12 @@ library(magrittr)
 tickers = c("JNJ", "CBNK", "MMM", "CAT", "AAPL", "NFLX", "AMZN", "GOOG", "PEP", "FB", "NVDA", "BABA", "ACC", "AAL", "ABNB", "ADBE", "ADSK", "APR", "ASTE", "ATHE")
 #get from last 20 years
 getSymbols(tickers, src = "yahoo", from = "2001-11-01", to = "2021-11-01")
-
 summary(MMM)
 #plot
-candleChart(MMM, up.col = "green", dn.col = "red", theme = "white")
+#ta=null es para quitar los numeritos en rojo 
+candleChart(MMM, up.col = "green", dn.col = "red", theme = "white", TA = NULL)
 #calculate the 10-,50-,200 moving averages 
 candleChart(MMM, up.col = "green", dn.col = "red", theme = "white")
 addSMA(n = c(20, 50, 200))
+
+plot(MMM$MMM.Adjusted)
